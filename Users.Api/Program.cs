6569dee3365f792a28a897e0f170c09668c5dc43
ajaybhalerao12 <ajaybhalerao12@gmail.com>
2024-08-11 10:56:57 +0530

    feat: Add Yarp proxy

--- Users.Api/Program.cs
+++ Users.Api/Program.cs
@@ -1,6 +1,32 @@
+using Bogus;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+var persons = Enumerable.Range(1, 100).Select(num => new { Id = num, Name = new Faker().Person.FullName }).ToArray();
+
+app.MapGet("/users", (string? filter) => persons.Where(p => string.IsNullOrWhiteSpace(filter) || p.Name.Contains(filter)));
+
+app.MapGet("/users/{id}", (int id) =>
+{
+    var person = persons.FirstOrDefault(p => p.Id == id);
+    return person is not null ? Results.Ok(person) : Results.NotFound();
+});
+
+//app.MapGet("/", () => "Hello World Users Api!");
+
+app.UseHttpsRedirection();
 
 app.Run();
--- Yarp.Gateway/Program.cs
+++ Yarp.Gateway/Program.cs
@@ -1,6 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services
+    .AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapReverseProxy();
+
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
